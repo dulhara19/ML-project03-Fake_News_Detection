1ab2adcb48db59ebfb57fb4e17e769c9caeecdf8
dulhara <wkldulhara@gmail.com>
2025-03-16 00:24:01 +0530

    imported important libs

---
+++ model.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import re
+import string
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+
+# Load the dataset
+df = pd.read_csv("news.csv")
