e222dc8dce39d8dd43e890f83dce93002c28c820
dulhara <wkldulhara@gmail.com>
2025-03-16 01:44:39 +0530

    data cleaned now and ready to train

--- model.py
+++ model.py
@@ -18,27 +18,33 @@ print("-------------------------------------------------------------------")
 df = df.drop(columns=['index']) 
 print(df)
 
-
+print("-------------------------------------------------------------------")
 
 # Combine both fake and real datasets (if separate)
 df['label'] = df['label'].map({'FAKE': 0, 'REAL': 1})  # Convert labels to 0/1
+print(df)
 
-df['content'] = df['title'] + " " + df['text']  # Merge title & text
-df['content'] = df['content'].astype(str)  # Ensure all data is string type
+print("-------------------------------------------------------------------")
 
+# df['content'] = df['title'] + " " + df['text']  # Merge title & text
+df['content'] = df['title'] + ' ' + df['text']
+print(df)
+# df['content'] = df['content'].astype(str)  # Ensure all data is string type
+print("-------------------------------------------------------------------")
+df = df[['title', 'text', 'content', 'label']]
+print(df)
+print(df['label'].isnull().sum())  # This will show how many missing labels you have
 
-
+print("-------------------------------------------------------------------")
 # Text Preprocessing Function
 def clean_text(text):
     text = text.lower()  # Lowercase
     text = re.sub(r'\[.*?\]', '', text)  # Remove text in brackets
     text = re.sub(r'http\S+|www\S+', '', text)  # Remove URLs
     text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)  # Remove punctuation
-    text = re.sub(r'\d+', '', text)  # Remove numbers
+    # text = re.sub(r'\d+', '', text)  # Remove numbers
     return text
 
 df['content'] = df['content'].apply(clean_text)  # Apply text cleaning
-
-
 print(df)
-print(df['label'].isnull().sum())  # This will show how many missing labels you have
+
